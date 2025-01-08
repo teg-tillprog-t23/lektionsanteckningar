2446676105ecf0c1ef6adbda8d9d2d05377c8697
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-01-08 10:00:53 +0100

    lösningsförslag övningsuppgift

--- Spelprogrammering/repetition_python_1.py
+++ Spelprogrammering/repetition_python_1.py
@@ -8,3 +8,26 @@
 # Det enda som ska hända när programmet körs är att huvudfunktionen kallas
 
 # Inga globala variabler får finnas
+
+def kolla_mening(mening):
+    vokaler = ("a", "o", "u", "å", "e", "i", "y", "ä", "ö")
+    lista_med_ord = []
+    ord_i_mening = mening.split()
+    for ordet in ord_i_mening:
+        antal_vokaler = 0
+        for bokstav in ordet:
+            if bokstav.lower() in vokaler:
+                antal_vokaler += 1
+        if antal_vokaler > 3:
+            lista_med_ord.append(ordet)
+    
+    return lista_med_ord
+
+def main():
+    mening = input("Skriv in din mening: ")
+    lista_att_skriva = kolla_mening(mening)
+    for element in lista_att_skriva:
+        print(element)
+
+main()
+

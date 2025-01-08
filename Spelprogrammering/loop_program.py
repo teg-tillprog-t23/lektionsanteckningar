ab10574faf3b3c67b8d1d35532756b36e437ba4e
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-01-08 08:59:57 +0100

    loop-program klart

--- Spelprogrammering/loop_program.py
+++ Spelprogrammering/loop_program.py
@@ -1,2 +1,18 @@
 # Ett loop-program som skriver ut ditt namn
+import time
 
+namn = "Felicia"
+senast_uppdaterad = time.time()
+nuvarande_bokstav = 0
+
+while True:
+    print(".", end = "")
+    if time.time()-senast_uppdaterad >= 1:
+        print(f"Ge mig ett {namn[nuvarande_bokstav]}")
+        nuvarande_bokstav += 1
+        senast_uppdaterad = time.time()
+    
+    if nuvarande_bokstav == 7:
+        break
+
+print(f"Det blir {namn.upper()}")
